a293e6bdfcf9419de8929607a8810fdbf3ef4ff9
Akshay <akshaysangani44@gmail.com>
2024-05-01 20:46:22 +0530

    index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/bookings", bookingRoutes)
 app.use("/users", userRoutes)
 
 /* MONGOOSE SETUP */
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 mongoose
   .connect(process.env.MONGO_URL, {
     dbName: "Dream_Nest",
@@ -28,6 +28,6 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    app.listen(port, () => console.log(`Server Port: ${port}`));
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
   })
   .catch((err) => console.log(`${err} did not connect`));
